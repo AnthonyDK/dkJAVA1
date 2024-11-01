ac3d1cc5f50957cb272a06c1188479ddbfcf9ecb
Keyqi <2545779360@qq.com>
2024-11-01 09:20:14 +0800

    之前鸿蒙的项目文件

---
+++ hormonyproject1/src/main/java/org/hbu/hormonyproject1/Hormonyproject1Application.java
@@ -0,0 +1,13 @@
+package org.hbu.hormonyproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hormonyproject1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hormonyproject1Application.class, args);
+    }
+
+}
---
+++ hormonyproject1/src/test/java/org/hbu/hormonyproject1/Hormonyproject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hbu.hormonyproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hormonyproject1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
